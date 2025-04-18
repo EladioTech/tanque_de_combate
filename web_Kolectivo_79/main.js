1b4e7db54b3da5942b77c6d3954fb21a76129f3c
eladio-rey <eladio.rey@aol.com>
2025-04-18 19:22:47 +0200

    modifico carpetas de lugar porque me estan dando fallo en las subidas a surge y netlify

---
+++ web_Kolectivo_79/main.js
@@ -0,0 +1,10 @@
+/*Me gustaría aprender a usarla para llamar la atención 
+de alguna parte esencial de la pág. web y que vaya desfalleciendo 
+suavemente para que no afecte viualmente a la estética de la pág.*/
+/*<script> 
+    function cerrarBombilla() {
+        const bombilla = document.getElementById('bombilla');
+        bombilla.style.opacity = '0';
+        setTimeout(() => bombilla.style.display = 'none', 500);
+    }
+</script>*/
