0c189fdf577cdedbbe1715386d15bc724d9c6fec
eladio-rey <eladio.rey@aol.com>
2025-04-18 14:02:03 +0200

    sigo preparando la pantalla de presentación y ahora entro con las entradas a pedidos y contacto

--- urgando FLEX/pruebas/web_Kolectivo_79/02_js/2_filas_4_columnas.js
+++ urgando FLEX/pruebas/web_Kolectivo_79/02_js/2_filas_4_columnas.js
@@ -1,4 +1,7 @@
-<script>
+/*Me gustaría aprender a usarla para llamar la atención 
+de alguna parte esencial de la pág. web y que vaya desfalleciendo 
+suavemente para que no afecte viualmente a la estética de la pág.*/
+<script> 
     function cerrarBombilla() {
         const bombilla = document.getElementById('bombilla');
         bombilla.style.opacity = '0';
