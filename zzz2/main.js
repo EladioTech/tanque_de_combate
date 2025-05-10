6603b30bceea8ae1189e240a62306529c4c35624
EladioTech <eladio.rey@aol.com>
2025-05-10 22:13:06 +0200

    cambio el diseño y el nombre de la web

---
+++ zzz2/main.js
@@ -0,0 +1,17 @@
+/*Me gustaría aprender a usarla para llamar la atención 
+de alguna parte esencial de la pág. web y que vaya desfalleciendo 
+suavemente para que no afecte viualmente a la estética de la pág.*/
+/*<script> 
+    function cerrarBombilla() {
+        const bombilla = document.getElementById('bombilla');
+        bombilla.style.opacity = '0';
+        setTimeout(() => bombilla.style.display = 'none', 500);
+    }
+</script>*/
+
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
